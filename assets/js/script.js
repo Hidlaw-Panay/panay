c26b51ef666bb3ad9738ea0ec9cf5c16dc682de6
Mark <markjungersaniva@gmail.com>
2022-08-27 15:40:59 +0800

    Added news page and blog feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,8 @@
+const url = '/assets/files/blogs.json';
+// const url = 'https://hidlaw-panay.github.io/panay/assets/files/blogs.json';
+let routeName;
+let blogs = [];
+let blogDiv;
 const header = document.querySelector('#header');
 const featured = document.querySelector('#featured');
 const services = document.querySelector('#services');
@@ -47,13 +52,97 @@ if(footer) {
 }
 
 window.onload = function() {
+  // set active nav-item
   var mainRoute = window.location.pathname.split('/')[1];
-  var routeName = mainRoute.split('.')[0];
+  routeName = mainRoute.split('.')[0];
   setTimeout(() => {
     let navItem = document.querySelector(`[data-route="${routeName}"]`);
 
     if(navItem) {
       navItem.classList.add('active')
     }
+
+    // set featured blog
+    blogDiv = document.getElementById('featured-blog');
+    if(blogDiv) {
+      fetch(url)
+      .then((blob) => blob.json())
+      .then((data) => blogs.push(...data));
+    }
   }, 250);
+
+
 }
+
+function displayData() {
+  if(blogDiv) {
+    if(routeName === 'index') {
+      blogs = blogs.slice(0,1)
+    }
+    const html = blogs
+      .sort((next, prev) => {
+        let prevDate = new Date(prev.dateCreated);
+        let nextDate = new Date(next.dateCreated);
+        return prevDate - nextDate;
+      })
+      .map((data) => {
+        return `
+          <article class="entry">
+            <div class="entry-img">
+              <img src="${data.img}" alt="" class="img-fluid" />
+            </div>
+
+            <h2 class="entry-title">
+              <a href="blog-single.html"
+                >${data.title}</a
+              >
+            </h2>
+
+            <div class="entry-meta">
+              <ul>
+                <li class="d-flex align-items-center">
+                  <i class="bi bi-person"></i>
+                  <a href="blog-single.html">${data.author}</a>
+                </li>
+                <li class="d-flex align-items-center">
+                  <i class="bi bi-clock"></i>
+                  <a href="blog-single.html"
+                    ><time datetime="2020-01-01">${data.dateCreated}</time></a
+                  >
+                </li>
+            </div>
+
+            <div class="entry-content">
+              <p>
+                ${data.content}
+              </p>
+              <div class="read-more">
+                <a href="${data.url}">Read More</a>
+              </div>
+            </div>
+          </article>
+        `;
+      })
+      .join('');
+    blogDiv.innerHTML = html;
+  }
+  
+}
+
+function resolveAfter2Seconds() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('resolved');
+    }, 2000);
+  });
+}
+
+async function asyncCall() {
+  console.log('calling');
+  const result = await resolveAfter2Seconds();
+  console.log(result);
+  displayData();
+  // expected output: "resolved"
+}
+
+asyncCall();
